c816a6f67c5f5e9ca61ccce6153e08957aaaba4f
Stanislav Besedin <bes_1004@mail.ru>
2024-06-15 01:22:13 +0300

    Дубль два. Не понял, как методы clearSubtask и removeSubtask реализовать в рамках эпика.

--- src/Data/Epic.java
+++ src/Data/Epic.java
@@ -1,11 +1,9 @@
 package Data;
 
 import java.util.ArrayList;
-import TaskManager.TaskManager;
 
 public class Epic extends Task {
     ArrayList<Integer> subTaskIds;
-    TaskManager taskManager = new TaskManager();
 
     public Epic(String taskDescription, String taskName) {
         super(taskDescription, taskName);
@@ -18,19 +16,6 @@ public class Epic extends Task {
 
     public ArrayList<Integer> getSubTaskIds() {
         return subTaskIds;
-    }
-
-
-    public void clearSubTasks() {
-        subTaskIds.clear();
-        taskManager.clearSubTasks();
-    }
-
-    public void removeSubTaskById(int id) {
-        if (subTaskIds.contains(id)) {
-            subTaskIds.remove(id);
-            taskManager.removeSubTaskById(id);
-        }
     }
 }
 
